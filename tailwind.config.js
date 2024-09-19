23945f5448671b06d2ce017f9a906c837235873d
Mohammad Tohidul Hasan <tohidul3216@gmail.com>
2024-09-19 21:26:44 +0600

    fix(configure): update Tailwind to use content instead of purge, remove variants

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ["./src/**/*.{js, jsx, ts, tsx}", "./public/index.html"],
+  content: ["./src/**/*.{js, jsx, ts, tsx}", "./public/index.html"],
   darkMode: "class", // Enable dard mode with class strategy
-  content: [],
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
