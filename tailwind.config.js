5acb29e636bcc050292fbf50345a4e22d9e741e6
Mohammad Tohidul Hasan <tohidul3216@gmail.com>
2024-09-19 15:10:58 +0600

    build: configure Tailwind CSS and PostCSS

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ["./src/**/*.{js, jsx, ts, tsx}", "./public/index.html"],
+  darkMode: "class", // Enable dard mode with class strategy
+  content: [],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
