eacf0f6394a0fcb4ecea543e73373a5a5a5d7e02
Mohammad Tohidul Hasan <tohidul3216@gmail.com>
2024-09-19 20:14:58 +0600

    style: add Tailwind CSS classes to NewsCard component

--- src/components/NewsCard.js
+++ src/components/NewsCard.js
@@ -1,10 +1,10 @@
 import React from "react";
 
 const NewsCard = ({article}) => (
-    <div>
-        <h2>{article.title}</h2>
-        <p>{article.description}</p>
-        <a href={article.url} target="_blank">Read More</a>
+    <div className="bg-white dark:bg-gray-800 p-4 rounded-lg shadow-md">
+        <h2 className="text-lg font-bold text-gray-800 dark:text-gray-100">{article.title}</h2>
+        <p className="text-gray-600 dark:text-gray-400">{article.description}</p>
+        <a className="text-blue-500 hover:underline" href={article.url} target="_blank">Read More</a>
     </div>
 );
 
