e083a34258bddde906c7e44b590e20aedc02c97a
Mohammad Tohidul Hasan <tohidul3216@gmail.com>
2024-09-19 17:44:35 +0600

    style: add Tailwind CSS utility classes to Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,8 @@
 import React from "react";
 
 const Header = () => (
-    <header>
-        <h1>News Aggregator</h1>
+    <header className="bg-blue-600 p-4">
+        <h1 className="text-white text-2xl font-bold">News Aggregator</h1>
     </header>
 );
 
